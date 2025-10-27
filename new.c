555c5ce3174a4a8bf5e285d916bee296279df43e
Vaibhav <vaibhavjadhav222007@gmail.com>
2025-10-27 11:50:58 +0530

    updated code

---
+++ new.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello Vaibhav! 👋\n");
+    printf("This is your first Git push test from VS Code.\n");
+    printf("Keep coding and learning every day!\n");
+    return 0;
+}
