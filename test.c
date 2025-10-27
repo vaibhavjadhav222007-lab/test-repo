1b95a8ac82f422a4e899844f9f8db1c280b1668c
Vaibhav <vaibhavjadhav222007@gmail.com>
2025-10-27 11:34:34 +0530

    My first test push from VS Code

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, GitHub from VS Code!\n");
+    printf("This is my first test push.\n");
+
+    int a = 5, b = 3;
+    printf("Sum of %d and %d is %d\n", a, b, a + b);
+
+    return 0;
+}
